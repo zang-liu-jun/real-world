6bfc2782cf974aaf0af726dd7ecbdfd4d14b26b7
Zliuj <2417170480@qq.com>
2022-04-08 09:51:07 +0800

    refactor:优化跳转逻辑

--- src/components/main-header/main-header.vue
+++ src/components/main-header/main-header.vue
@@ -2,11 +2,11 @@
   <div class="header-box">
     <div class="main-box">
       <div style="display: flex; justify-content: flex-start">
-        <div class="logo" @click="goHome">
+        <div class="logo" @click="getIn('home')">
           <span>RealWorld</span>
         </div>
         <div class="main-navigation">
-          <div v-for="(item,index) in routesObj" class="nav-item" @click="getInItem(item.routeName)">
+          <div v-for="(item,index) in routesObj" class="nav-item" @click="getIn(item.routeName)">
             <span :class="currentItem===item.routeName?'active-item':''">{{
                 item.name
               }}</span>
@@ -19,13 +19,13 @@
           <UserAvatar :src="userStore.user.image"></UserAvatar>
           <template #dropdown>
             <el-dropdown-menu>
-              <el-dropdown-item @click="getInPublish">写文章</el-dropdown-item>
-              <el-dropdown-item @click="getInUser">我的主页</el-dropdown-item>
+              <el-dropdown-item @click="getIn('publish')">写文章</el-dropdown-item>
+              <el-dropdown-item @click="getIn('user')">我的主页</el-dropdown-item>
               <el-dropdown-item @click="logout">注销</el-dropdown-item>
             </el-dropdown-menu>
           </template>
         </el-dropdown>
-        <div v-else class="login" @click="getInLogin">
+        <div v-else class="login" @click="getIn('service')">
           <span>登录</span>
         </div>
       </div>
@@ -43,15 +43,8 @@ import {useUserStore} from "@/store";
 const router = useRouter()
 const userStore = useUserStore()
 
+//路由跳转
 let currentItem = ref("")
-
-async function goHome() {
-  await router.replace({
-    name: "home"
-  })
-  location.reload()
-}
-
 const routesObj = [
   {
     name: "首页",
@@ -63,23 +56,15 @@ const routesObj = [
   }
 ]
 
-async function getInItem(name) {
-  await router.push({
+function getIn(name) {
+  currentItem.value = name
+  router.push({
     name
-  });
-  currentItem.value = router.currentRoute.value.name as string;
+  })
 }
 
 //search
 let searchValue = ref<string>("")
-
-
-async function getInPublish() {
-  await router.push({
-    name: "publish"
-  })
-  location.reload()
-}
 
 function logout() {
   localStorage.removeItem("user");
@@ -87,18 +72,6 @@ function logout() {
   userStore.$reset();
 }
 
-function getInLogin() {
-  router.push({
-    name: "login"
-  })
-}
-
-async function getInUser() {
-  await router.push({
-    name: "user"
-  })
-  location.reload()
-}
 </script>
 
 <style lang="scss" scoped>
